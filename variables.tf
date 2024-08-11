0735d1fe16eb15d9492686c06eb1e71c064b8e75
Shenon69 <ask.tikki69@gmail.com>
2024-08-11 15:04:42 +0530

    feat: Setup VCS automation

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
   default = "trishan"
   type    = string
 }
+
+variable "github_app_installation_id" {
+  default = 53681835
+  type    = number
+}
+
+variable "github_organization_name" {
+  default = "Shenon69"
+  type    = string
+}
